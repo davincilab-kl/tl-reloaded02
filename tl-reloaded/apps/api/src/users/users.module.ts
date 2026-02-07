35460ccc82b1c2fd455c66e2ef08ccd39c4212df
Peter Gawin <petergawin@MacBook-Pro-von-Peter-2.local>
2026-02-07 08:57:55 +0100

    fix: import StorageModule in UsersModule

--- tl-reloaded/apps/api/src/users/users.module.ts
+++ tl-reloaded/apps/api/src/users/users.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaService } from '../prisma.service';
+import { StorageModule } from '../storage/storage.module';
 
 @Module({
+    imports: [StorageModule],
     controllers: [UsersController],
     providers: [UsersService, PrismaService],
     exports: [UsersService],
